5b9ae8364c4d141269c991658050eff9cc103002
kmk9000 <kallekoivuniemi@gmail.com>
2025-03-28 13:04:16 +0200

    readme

---
+++ viikko_1/js/script.js
@@ -0,0 +1,14 @@
+const muuttuja = "Hello World";
+
+function funktio() {
+  let p = 5 * 6;
+  let x, y, z;
+  x = 5;
+  y = 6;
+  z = x + y;
+  console.log("This is z: ", z);
+  let toinen = "Tänne tulee arvo";
+  console.log(toinen);
+}
+
+funktio();
---
+++ viikko_1/js/script_oma.js
@@ -0,0 +1,13 @@
+const variable = "What is your age";
+
+function funktio() {
+  let x, y, z;
+  x = 1991;
+  y = 2025;
+  z = y - x;
+  console.log("Your age is ", z);
+  let toinen = "Tässä on arvo";
+  console;
+}
+
+funktio();
